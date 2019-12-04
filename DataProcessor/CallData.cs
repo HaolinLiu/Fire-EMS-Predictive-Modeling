79e801da796278ac6bd706d0cbb967ca7c3ffe1e
Anna Murphy <anna.liz.murphy@gmail.com>
2019-12-04 10:14:05 -0800

    created empty call data class (loosing access to laptop)

---
+++ DataProcessor/CallData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataProcessor
+{
+    /// <summary>
+    /// Class to hold call data
+    /// </summary>
+    public class CallData
+    {
+       /// <summary>
+       ///
+       /// </summary>
+    }
+}
