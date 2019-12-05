60b8587d5da34eac876184f7168fd04e91f1c029
Anna Murphy <anna.liz.murphy@gmail.com>
2019-12-05 11:21:01 -0800

    Implamented data reader class (untested)

--- DataProcessor/CallData.cs
+++ DataProcessor/CallData.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Data;
 using System.Text;
 
 namespace DataProcessor
@@ -9,8 +11,35 @@ namespace DataProcessor
     /// </summary>
     public class CallData
     {
-       /// <summary>
-       ///
-       /// </summary>
+        //public DataTable CallDataTable;
+
+        public static DataTable ImportCallData(string fileName)
+        {
+            DataTable newDataTable = new DataTable();
+
+            using (StreamReader streamReader = new StreamReader(fileName))
+            {
+                string[] colNames = streamReader.ReadLine().Split(',');
+                foreach (string colName in colNames)
+                {
+                    newDataTable.Columns.Add(colName);
+                }
+
+                while (!streamReader.EndOfStream)
+                {
+                    string[] row = streamReader.ReadLine().Split(',');
+                    DataRow newDataRow = newDataTable.NewRow();
+                    for (int i = 0; i < colNames.Length; i++)
+                    {
+                        newDataRow[i] = row[i];
+                    }
+                    newDataTable.Rows.Add(newDataRow);
+                }
+            }
+
+            return newDataTable;
+        }
+
     }
+
 }
