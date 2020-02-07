278339d8b1afb2cb5ed16c7161b47b8e1f6548a3
Anna Murphy <murphann@oregonstate.edu>
2020-02-06 17:43:01 -0800

    created CallResponseData datatype

---
+++ DataProcessor/CallResponseData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataProcessor
+{
+    struct CallResponseData
+    {
+        struct Response {
+
+        string UnitID;
+        string UnitType;
+        DateTime Dispatched;
+        DateTime Arrived;
+
+        }
+
+        string CallID;
+    }
+}
