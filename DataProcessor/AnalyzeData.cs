8b7e6a0a368d77e70e0c1f71369c3bd8ce4dc20e
Anna Murphy <murphann@oregonstate.edu>
2020-02-11 13:40:16 -0800

    Commented merge method

--- DataProcessor/AnalyzeData.cs
+++ DataProcessor/AnalyzeData.cs
@@ -9,18 +9,22 @@ namespace DataProcessor
 	{
         
         public static CallResponseData[] CombineData(DataTable callData, DataTable responseData)
-		{
-			Console.WriteLine("Number of datapoints: " + callData.Rows.Count);
+		{	
+			//Array of data objects
 			CallResponseData[] data = new CallResponseData[callData.Rows.Count];
+			
+			//Counter to track what data item we are on
 			int rowCount = 0;
 
 			foreach (DataRow row in callData.Rows)
 			{
+				//read the relevent data out of the data table
 				string callID = row["CallID"].ToString();
 				string natureCode = row["NatureCode"].ToString();
 				DateTime callTime = DateTime.Parse(row["Date"].ToString());
 				string address = row["Address"].ToString();
 
+				//Get all response entries for the current call
 				DataRow[] responsesToCall = responseData.Select("CallID = '" + callID.ToString() + "'");
 
 				List<CallResponseData.Response> responses = new List<CallResponseData.Response>();
@@ -36,6 +40,7 @@ namespace DataProcessor
 					responses.Add(response);
 				}
 
+				//create the new object and store it in the array at the relevent spot
 				CallResponseData newData = new CallResponseData(callID, natureCode, callTime, address, responses);
 				data[rowCount] = newData;
 				rowCount++;
